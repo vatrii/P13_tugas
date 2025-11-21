9df25194a438de0d6386000767dbf73eefef9ff1
unknown <hasnawidasari15@gmail.com>
2025-11-21 11:22:23 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$nama = "Website Wisata Sederhana";
+$wisata = ["Borobudur", "Kuta", "TMII"];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $nama ?></title>
+</head>
+<body>
+
+<h1><?= $nama ?></h1>
+
+<h3>Daftar Tempat Wisata:</h3>
+<ul>
+    <?php foreach ($wisata as $w) : ?>
+        <li><?= $w ?></li>
+    <?php endforeach; ?>
+</ul>
+
+</body>
+</html>
